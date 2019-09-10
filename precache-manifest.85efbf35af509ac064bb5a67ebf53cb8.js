48461ef34e2c74ead1801ea8d95ea13ccfa21555
Shyam <bshyam@visteon.com>
2019-09-10 10:15:20 +0530

    Updates

---
+++ precache-manifest.85efbf35af509ac064bb5a67ebf53cb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1112bd96c54b5271bdd2381830ea52e7",
+    "url": "/AdiShyam.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "83bbc4feb5bffe5d9ed4",
+    "url": "/AdiShyam.github.io/monsters-rolodex/static/css/main.bbff778f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/AdiShyam.github.io/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "83bbc4feb5bffe5d9ed4",
+    "url": "/AdiShyam.github.io/monsters-rolodex/static/js/main.e06771a9.chunk.js"
+  },
+  {
+    "revision": "6f85d7d52cdfdd53cf5b",
+    "url": "/AdiShyam.github.io/monsters-rolodex/static/js/runtime~main.693e329d.js"
+  }
+]);
